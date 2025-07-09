844abb618f004b210ee4e8b5de94f45e23aecc2d
madison.hersom.ctr-at-661095517404 <madison.hersom.ctr@ad.absolutegov.net>
2025-07-09 15:54:12 -0400

    testing

--- modules/conformance-packs/outputs.tf
+++ modules/conformance-packs/outputs.tf
@@ -1,17 +1,17 @@
 # Output the names of organization-level conformance packs
 output "org_conformance_pack_names" {
   description = "Names of the AWS Config organization-level conformance packs deployed"
-  value       = var.is_org ? aws_config_organization_conformance_pack.this[*].name : []
+  value       = var.is_org ? aws_config_organization_conformance_pack.conformance_packs[*].name : []
 }
 
 # Output the names of standalone conformance packs
 output "standalone_conformance_pack_names" {
   description = "Names of the AWS Config standalone conformance packs deployed"
-  value       = var.is_org ? [] : aws_config_conformance_pack.this[*].name
+  value       = var.is_org ? [] : aws_config_conformance_pack.conformance_packs[*].name
 }
 
 # Optional: Output all conformance pack ARNs (org or standalone)
 output "conformance_pack_arns" {
   description = "ARNs of deployed AWS Config conformance packs"
-  value       = var.is_org ? aws_config_organization_conformance_pack.this[*].arn : aws_config_conformance_pack.this[*].arn
+  value       = var.is_org ? aws_config_organization_conformance_pack.conformance_packs[*].arn : aws_config_conformance_pack.conformance_packs[*].arn
 }
