8e221974655877f53c1bcdd1b7779621a2865171
Kendall Trudick <ktrudickm@gmail.com>
2025-08-27 09:52:15 -0700

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
 output "fedramp_conformance_pack_arn" {
-  value = aws_config_conformance_pack.conformance_packs[0].arn
+  value = try(aws_config_conformance_pack.conformance_packs[0].arn, null)
 }
 
 output "nist_conformance_pack_arn" {
-  value = aws_config_conformance_pack.conformance_packs[1].arn
+  value = try(aws_config_conformance_pack.conformance_packs[1].arn, null)
 }
