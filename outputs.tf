db6f13c09064442e2ba72e2010a564223366e2ab
Kendall Trudick <ktrudickm@gmail.com>
2025-09-02 10:26:34 -0700

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -5,8 +5,3 @@ output "fedramp_conformance_pack_arn" {
 output "nist_conformance_pack_arn" {
   value = try(aws_config_conformance_pack.conformance_packs[1].arn, null)
 }
-
-output "config_role_arn" {
-  description = "ARN of the AWS Config IAM role"
-  value       = var.create_iam_role ? aws_iam_role.custom_aws_config_role[0].arn : var.existing_config_role_arn
-}
