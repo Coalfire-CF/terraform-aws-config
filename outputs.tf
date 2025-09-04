bef9031e87a29491b40805fe685524a4488271b3
Matthew Pappas <matthew.pappas@coalfire.com>
2025-09-04 11:22:33 -0700

    org conformance

--- outputs.tf
+++ outputs.tf
@@ -6,10 +6,10 @@
 #   value = try(aws_config_conformance_pack.conformance_packs[1].arn, null)
 # }
 
-output "fedramp_conformance_pack_arn" {
-  value = try(aws_config_organization_conformance_pack.conformance_packs[0].arn, null)
-}
+# output "fedramp_conformance_pack_arn" {
+#   value = try(aws_config_organization_conformance_pack.conformance_packs[0].arn, null)
+# }
 
-output "nist_conformance_pack_arn" {
-  value = try(aws_config_organization_conformance_pack.conformance_packs[1].arn, null)
-}
+# output "nist_conformance_pack_arn" {
+#   value = try(aws_config_organization_conformance_pack.conformance_packs[1].arn, null)
+# }
