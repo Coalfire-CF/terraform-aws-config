af90b073eb0d3a1be0a3463b2f87c777e1670b99
Matthew Pappas <matthew.pappas@coalfire.com>
2025-09-04 21:27:45 -0700

    config

--- config.tf
+++ config.tf
@@ -1,37 +1,37 @@
-resource "aws_config_configuration_recorder" "config" {
-  name     = "${var.resource_prefix}-config"
-  role_arn = aws_iam_role.custom_aws_config_role.arn
+# resource "aws_config_configuration_recorder" "config" {
+#   name     = "${var.resource_prefix}-config"
+#   role_arn = aws_iam_role.custom_aws_config_role.arn
 
-  recording_group {
-    all_supported                 = true
-    include_global_resource_types = true
-  }
+#   recording_group {
+#     all_supported                 = true
+#     include_global_resource_types = true
+#   }
 
-}
+# }
 
-resource "aws_sns_topic" "config_delivery" {
-  count             = data.aws_region.current.name == "us-gov-west-1" ? 1 : 0
-  name              = "${var.resource_prefix}-sns-config"
-  kms_master_key_id = var.sns_kms_key_id
-}
+# resource "aws_sns_topic" "config_delivery" {
+#   count             = data.aws_region.current.name == "us-gov-west-1" ? 1 : 0
+#   name              = "${var.resource_prefix}-sns-config"
+#   kms_master_key_id = var.sns_kms_key_id
+# }
 
-resource "aws_config_delivery_channel" "config" {
-  name           = "${var.resource_prefix}-config-delivery-${data.aws_region.current.name}"
-  s3_bucket_name = var.s3_config_id
-  sns_topic_arn  = length(aws_sns_topic.config_delivery) > 0 ? aws_sns_topic.config_delivery[0].arn : null
+# resource "aws_config_delivery_channel" "config" {
+#   name           = "${var.resource_prefix}-config-delivery-${data.aws_region.current.name}"
+#   s3_bucket_name = var.s3_config_id
+#   sns_topic_arn  = length(aws_sns_topic.config_delivery) > 0 ? aws_sns_topic.config_delivery[0].arn : null
 
-  snapshot_delivery_properties {
-    delivery_frequency = var.delivery_frequency
-  }
+#   snapshot_delivery_properties {
+#     delivery_frequency = var.delivery_frequency
+#   }
 
-}
+# }
 
-resource "aws_config_configuration_recorder_status" "config" {
-  name       = aws_config_configuration_recorder.config.name
-  is_enabled = true
+# resource "aws_config_configuration_recorder_status" "config" {
+#   name       = aws_config_configuration_recorder.config.name
+#   is_enabled = true
 
-  depends_on = [aws_config_delivery_channel.config]
-}
+#   depends_on = [aws_config_delivery_channel.config]
+# }
 
 # resource "aws_config_conformance_pack" "conformance_packs" {
 #   count = var.create_conformance_packs ? length(var.conformance_pack_names) : 0
@@ -44,7 +44,7 @@ resource "aws_config_configuration_recorder_status" "config" {
 # }
 
 # resource "aws_config_organization_conformance_pack" "conformance_packs" {
-#   count = var.create_conformance_packs ? length(var.conformance_pack_names) : 0
+#   count = var.create_conformance_packs && var.is_org ? length(var.conformance_pack_names) : 0
 
 #   name               = var.conformance_pack_names[count.index]
 #   delivery_s3_bucket = var.s3_config_id
