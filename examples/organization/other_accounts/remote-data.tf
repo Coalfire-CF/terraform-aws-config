3db7ed909c3e93b69faa83fb7dbb7059548b689d
mhersom <madison.hersom@coalfire.com>
2026-03-02 11:16:21 -0500

    provider updates

--- examples/organization/other_accounts/remote-data.tf
+++ examples/organization/other_accounts/remote-data.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket       = "client-mgmt-us-gov-west-1-tf-state" # Update to point to management plane TF State bucket
-    region       = "us-gov-west-1"
-    key          = "client-aws-mgmt/us-gov-west-1/aws-config.tfstate" # May need to update path to correctly point to management plane account tfstate bucket
-    encrypt      = true
+    bucket  = "client-mgmt-us-gov-west-1-tf-state" # Update to point to management plane TF State bucket
+    region  = "us-gov-west-1"
+    key     = "client-aws-mgmt/us-gov-west-1/aws-config.tfstate" # May need to update path to correctly point to management plane account tfstate bucket
+    encrypt = true
 
     assume_role = {
       role_arn     = "arn:aws-us-gov:iam::<mgmt_account_id>:role/OrganizationAccountAccessRole" #update mgmt account id
@@ -19,6 +19,6 @@ data "terraform_remote_state" "fedramp_mgmt_account_setup" {
   config = {
     bucket = "client-mgmt-${var.aws_region}-tf-state"
     region = var.aws_region
-    key    = "client-mgmt/${var.aws_region}/account-setup.tfstate" 
+    key    = "client-mgmt/${var.aws_region}/account-setup.tfstate"
   }
 }
